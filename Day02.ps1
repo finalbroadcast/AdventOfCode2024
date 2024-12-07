7fdddb65368136b67d785921525d94b5e6f38bd0
finalbroadcast <finalbroadcast@gmail.com>
2024-12-07 02:36:15 -0600

    Update Day02.ps1
    Changed danger checks to function to better support the checks in Day 2.

--- Day02.ps1
+++ Day02.ps1
@@ -1,49 +1,96 @@
+$SafetyInput = Get-Content "AdventOfCode2024/Day02input.txt"
 $DangerousCount = 0
 $SafeCount = 0
-$SafetyReports = Get-Content "H:\My_Workspace\Powershell-Scripts\Advent of Code 2024\Day02-Input.txt"
-forEach($report in $SafetyReports){
-   $thisCheck = $report.Split(' ')
-   $thisState = $null
-   for($i=0;$i -le $($thisCheck.Length-1);$i++){
-        if($i -eq $thisCheck.Length-1){
-            $SafeCount++
-            Write-Host "$thisCheck is Safe"
-            Break
+function Check-Safety{
+    param(
+        [int] $val1,
+        [int] $val2,
+        [String] $Direction = ""
+    )
+    $CheckedValue = $val1 - $val2
+    $DangerStatus = 0
+    $FlowDanger = 1 
+    $JumpDanger = 1
+    if($checkedValue -eq 0){
+        $DangerStatus = 1
+        $CheckResults = [PSCustomObject]@{
+            DangerStatus = $DangerStatus
+            Direction = $Direction
         }
-        $CheckResult = $thisCheck[$i] - $thisCheck[$i+1]
-        Write-Host "Change is $CheckResult"
-        if(($CheckResult -lt -3) -or ($CheckResult -gt 3)){
-            $DangerousCount ++
-            Write-Host "Change is greater than 3, $thisCheck is Dangerous"
-            Break
-        }
-        if($CheckResult -eq 0){
-            $DangerousCount ++
-            Write-Host "No change, $thisCheck is Dangerous"
-            Break
-        }
-        if(($null -eq $thisState) -and ($CheckResult -ge 1)){
-            $thisState = "Increasing"
-        }
-        if(($null -eq $thisState) -and ($CheckResult -le -1)){
-            $thisState = "Decreasing"
-        }
-        if(($i -ne 0) -and ("Decreasing" -eq $thisState) -and ($CheckResult -ge 1)){
-            $DangerousCount ++
-            Write-Host "Was decreasing now increasing, $thisCheck is Dangerous"
-            Break
-        }
-        if(($i -ne 0) -and ("Increasing" -eq $thisState) -and ($CheckResult -le -1)){
-            $DangerousCount ++
-            Write-Host "Was increasing now decreasing, $thisCheck is Dangerous"
-            Break
-        }
+        Return $CheckResults
     }
-    
+    if($direction -eq ""){
+        $FlowDanger = 0
+        if($checkedValue -lt 0){
+            $Direction = "Increasing"
+        }
+        elseif($checkedValue -gt 0){
+            $Direction = "Decreasing"
+        }  
+    }
+    if($Direction -eq "Increasing" -and ($CheckedValue -gt 0)){
+        $flowDanger = 1
+    }
+    elseif ($Direction -eq "Decreasing" -and ($CheckedValue -lt 0)){
+        $FlowDanger = 1
+    }
+    else{
+        $FlowDanger = 0
+    }
+    if((($CheckedValue -le 3) -and ($CheckedValue -gt 0)) -or (($CheckedValue -ge -3) -and ($CheckedValue -lt 0)))
+    {
+        $JumpDanger = 0    
+    }
+    elseIf($checkedValue -ge 4 -or $checkedValue -le -4){
+        $JumpDanger = 1
+        }
+    if($JumpDanger -eq 1 -or $FlowDanger -eq 1){
+        $DangerStatus = 1
+        $CheckResults = [PSCustomObject]@{
+            DangerStatus = $DangerStatus
+            Direction = $Direction
+        }
+        Return $CheckResults
+    }
+    else{
+        $DangerStatus = 0
+        $CheckResults = [PSCustomObject]@{
+            DangerStatus = $DangerStatus
+            Direction = $Direction
+        }
+        Return $CheckResults
+    }
+    }
+
+forEach($check in $SafetyInput){
+    $thisCheck = New-Object System.Collections.ArrayList
+    $thisDanger = 0
+    $thisDirection = ""
+    foreach($element in $check.Split(' ')){$thisCheck.add($element)}
+    for($i =0;$i -le $($thisCheck.count -1);$i++){
+        if($thisDanger -eq 1){
+            Write-Host "$thisCheck not safe"
+            $DangerousCount++
+            Break
+        }
+        if($i -eq $($thisCheck.Count-1)){
+            Write-Host "$thisCheck is safe"
+            $SafeCount++
+            Break
+        }
+        if(($i -eq 0) -and ($thisDirection -eq "") ){
+            $Result = Check-Safety -val1 $thisCheck[$i] -val2 $thisCheck[$i+1]
+        }
+        else{
+            $Result = Check-Safety -val1 $thisCheck[$i] -val2 $thisCheck[$i+1] -Direction $thisDirection
+        }
+        $thisDanger = $Result.DangerStatus
+        $thisDirection = $Result.Direction
+    }
 }
-if($DangerousCount + $SafeCount -eq $SafetyReports.Count){
-    Write-output "Out of $($SafetyReports.Count) there are $DangerousCount dangerous reports and $SafeCount safe reports"
+if($DangerousCount + $SafeCount -ne $SafetyInput.Count){
+    Write-Error "$DangerousCount + $SafeCount  do not equal $($SafetyInput.Count) check code"
 }
 else{
-    Write-Error "$DangerousCount dangerous reports and $SafeCount do not equal $SafetyReports.Count"
+    Write-Output "There are $DangerousCount dangerous checks and $Safecount safe checks."
 }
